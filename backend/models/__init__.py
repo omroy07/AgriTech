49966c81f4c036930042ca62387cf31019a6f1e3
Satyam Pandey <satyam53@tsecol.onmicrosoft.com>
2026-02-08 19:13:20 +0530

    Implement Micro-Insurance Underwriting & Automated Settlement System #1392

--- backend/models/__init__.py
+++ backend/models/__init__.py
@@ -7,24 +7,32 @@ from .prediction_history import PredictionHistory
 from .misc import Notification, File, YieldPool, PoolContribution, ResourceShare, PoolVote
 from .gews import DiseaseIncident, OutbreakZone, OutbreakAlert
 from .traceability import SupplyBatch, CustodyLog, QualityGrade, BatchStatus
-from .insurance import InsurancePolicy, ClaimRequest, RiskScoreHistory
+from .insurance import InsurancePolicy, ClaimRequest as LegacyClaim, RiskScoreHistory
 from .forum import ForumCategory, ForumThread, PostComment, Upvote, UserReputation
 from .knowledge import Question, Answer, KnowledgeVote, Badge, UserBadge, UserExpertise
 from .equipment import Equipment, RentalBooking, AvailabilityCalendar, PaymentEscrow
 from .farm import Farm, FarmMember, FarmAsset, FarmRole
 from .weather import WeatherData, CropAdvisory, AdvisorySubscription
 from .sustainability import CarbonPractice, CreditLedger, AuditRequest
+from .procurement import VendorProfile, ProcurementItem, BulkOrder, OrderEvent
+from .irrigation import IrrigationZone, SensorLog, ValveStatus, IrrigationSchedule
+from .processing import ProcessingBatch, StageLog, QualityCheck, ProcessingStage
+from .insurance_v2 import CropPolicy, ClaimRequest, PayoutLedger, AdjusterNote
 
 __all__ = [
     'User', 'UserRole', 'LoanRequest', 'PredictionHistory', 
     'Notification', 'File', 'YieldPool', 'PoolContribution', 
     'ResourceShare', 'PoolVote', 'DiseaseIncident', 'OutbreakZone', 'OutbreakAlert',
     'SupplyBatch', 'CustodyLog', 'QualityGrade', 'BatchStatus',
-    'InsurancePolicy', 'ClaimRequest', 'RiskScoreHistory',
+    'InsurancePolicy', 'LegacyClaim', 'RiskScoreHistory',
     'ForumCategory', 'ForumThread', 'PostComment', 'Upvote', 'UserReputation',
     'Question', 'Answer', 'KnowledgeVote', 'Badge', 'UserBadge', 'UserExpertise',
     'Equipment', 'RentalBooking', 'AvailabilityCalendar', 'PaymentEscrow',
     'Farm', 'FarmMember', 'FarmAsset', 'FarmRole',
     'WeatherData', 'CropAdvisory', 'AdvisorySubscription',
-    'CarbonPractice', 'CreditLedger', 'AuditRequest'
+    'CarbonPractice', 'CreditLedger', 'AuditRequest',
+    'VendorProfile', 'ProcurementItem', 'BulkOrder', 'OrderEvent',
+    'IrrigationZone', 'SensorLog', 'ValveStatus', 'IrrigationSchedule',
+    'ProcessingBatch', 'StageLog', 'QualityCheck', 'ProcessingStage',
+    'CropPolicy', 'ClaimRequest', 'PayoutLedger', 'AdjusterNote'
 ]
