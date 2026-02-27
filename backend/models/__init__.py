c85727bf60809886b92e9aa542c0954b88d5365e
Akanksha Trehun <akankshatrehun@gmail.com>
2026-02-27 22:35:20 +0530

    feat: Add government scheme finder with eligibility checker and application management
    - Added GovernmentScheme, SchemeApplication, and SchemeReminder models
    - Implemented eligibility checking based on farm size, income, and location
    - Created application assistance with document checklist and tips
    - Added deadline reminder system
    - Implemented search and filter functionality
    - Added API endpoints for schemes, applications, and reminders

--- backend/models/__init__.py
+++ backend/models/__init__.py
@@ -1,13 +1,27 @@
 """
 Database models package.
 """
+
 from .user import User, UserRole
 from .loan_request import LoanRequest
 from .prediction_history import PredictionHistory
-from .misc import Notification, File, YieldPool, PoolContribution, ResourceShare, PoolVote
+from .misc import (
+    Notification,
+    File,
+    YieldPool,
+    PoolContribution,
+    ResourceShare,
+    PoolVote,
+)
 from .gews import DiseaseIncident, OutbreakZone, OutbreakAlert
 from .traceability import SupplyBatch, CustodyLog, QualityGrade, BatchStatus
-from .insurance import InsurancePolicy, ClaimRequest as LegacyClaim, RiskScoreHistory, DynamicPremiumLog, RiskFactorSnapshot
+from .insurance import (
+    InsurancePolicy,
+    ClaimRequest as LegacyClaim,
+    RiskScoreHistory,
+    DynamicPremiumLog,
+    RiskFactorSnapshot,
+)
 from .forum import ForumCategory, ForumThread, PostComment, Upvote, UserReputation
 from .knowledge import Question, Answer, KnowledgeVote, Badge, UserBadge, UserExpertise
 from .equipment import Equipment, RentalBooking, AvailabilityCalendar, PaymentEscrow
@@ -16,22 +30,66 @@ from .alert import Alert, AlertPreference
 from .audit_log import AuditLog, UserSession
 from .media_payload import MediaPayload
 from .weather import (
-    WeatherData, CropAdvisory, AdvisorySubscription, RiskTrigger,
-    ClimateTelemetryEvent, ForceMajeureAlert, ParametricPolicyTrigger
+    WeatherData,
+    CropAdvisory,
+    AdvisorySubscription,
+    RiskTrigger,
+    ClimateTelemetryEvent,
+    ForceMajeureAlert,
+    ParametricPolicyTrigger,
 )
 from .weather import WeatherData, CropAdvisory, AdvisorySubscription, RiskTrigger
 from .sustainability import (
-    CarbonPractice, CreditLedger, AuditRequest, CarbonLedger,
-    EmissionSource, SustainabilityScore, ESGMarketListing
+    CarbonPractice,
+    CreditLedger,
+    AuditRequest,
+    CarbonLedger,
+    EmissionSource,
+    SustainabilityScore,
+    ESGMarketListing,
 )
 from .vendor_profile import VendorProfile
 from .procurement import ProcurementItem, BulkOrder, OrderEvent
-from .irrigation import IrrigationZone, SensorLog, ValveStatus, IrrigationSchedule, AquiferLevel, WaterRightsQuota
-from .processing import ProcessingBatch, StageLog, QualityCheck, ProcessingStage, SpectralScanData, DynamicGradeAdjustment
-from .insurance_v2 import CropPolicy, ClaimRequest, PayoutLedger, AdjusterNote, ParametricAutoSettlement
+from .irrigation import (
+    IrrigationZone,
+    SensorLog,
+    ValveStatus,
+    IrrigationSchedule,
+    AquiferLevel,
+    WaterRightsQuota,
+)
+from .processing import (
+    ProcessingBatch,
+    StageLog,
+    QualityCheck,
+    ProcessingStage,
+    SpectralScanData,
+    DynamicGradeAdjustment,
+)
+from .insurance_v2 import (
+    CropPolicy,
+    ClaimRequest,
+    PayoutLedger,
+    AdjusterNote,
+    ParametricAutoSettlement,
+)
 from .insurance_v2 import CropPolicy, ClaimRequest, PayoutLedger, AdjusterNote
-from .machinery import EngineHourLog, MaintenanceCycle, DamageReport, RepairOrder, AssetValueSnapshot, ComponentWearMap, MaintenanceEscrow
-from .soil_health import SoilTest, FertilizerRecommendation, ApplicationLog, RegenerativeFarmingLog, CarbonMintEvent
+from .machinery import (
+    EngineHourLog,
+    MaintenanceCycle,
+    DamageReport,
+    RepairOrder,
+    AssetValueSnapshot,
+    ComponentWearMap,
+    MaintenanceEscrow,
+)
+from .soil_health import (
+    SoilTest,
+    FertilizerRecommendation,
+    ApplicationLog,
+    RegenerativeFarmingLog,
+    CarbonMintEvent,
+)
 from .loan_v2 import RepaymentSchedule, PaymentHistory, DefaultRiskScore, CollectionNote
 from .warehouse import WarehouseLocation, StockItem, StockMovement, ReconciliationLog
 from .climate import ClimateZone, SensorNode, TelemetryLog, AutomationTrigger
@@ -39,8 +97,14 @@ from .labor import WorkerProfile, WorkShift, HarvestLog, PayrollEntry, LaborROIH
 from .logistics_v2 import DriverProfile, DeliveryVehicle, TransportRoute, FuelLog
 from .labor import WorkerProfile, WorkShift, HarvestLog, PayrollEntry
 from .logistics_v2 import (
-    DriverProfile, DeliveryVehicle, TransportRoute, FuelLog,
-    PhytoSanitaryCertificate, FreightEscrow, CustomsCheckpoint, GPSTelemetry
+    DriverProfile,
+    DeliveryVehicle,
+    TransportRoute,
+    FuelLog,
+    PhytoSanitaryCertificate,
+    FreightEscrow,
+    CustomsCheckpoint,
+    GPSTelemetry,
 )
 from .transparency import ProduceReview, PriceAdjustmentLog
 from .barter import BarterTransaction, BarterResource, ResourceValueIndex
@@ -53,84 +117,209 @@ from .circular import WasteInventory, BioEnergyOutput, CircularCredit
 from .carbon_escrow import CarbonTradeEscrow, EscrowAuditLog, CarbonCreditWallet
 from .disease import MigrationVector, ContainmentZone
 from .ledger import (
-    LedgerAccount, LedgerTransaction, LedgerEntry,
-    FXValuationSnapshot, Vault, VaultCurrencyPosition, FXRate,
-    AccountType, EntryType, TransactionType
+    LedgerAccount,
+    LedgerTransaction,
+    LedgerEntry,
+    FXValuationSnapshot,
+    Vault,
+    VaultCurrencyPosition,
+    FXRate,
+    AccountType,
+    EntryType,
+    TransactionType,
 )
+from .government_schemes import GovernmentScheme, SchemeApplication, SchemeReminder
 
 __all__ = [
     # Core
-    'User', 'UserRole', 'LoanRequest', 'PredictionHistory',
-    'Notification', 'File', 'YieldPool', 'PoolContribution',
-    'ResourceShare', 'PoolVote',
+    "User",
+    "UserRole",
+    "LoanRequest",
+    "PredictionHistory",
+    "Notification",
+    "File",
+    "YieldPool",
+    "PoolContribution",
+    "ResourceShare",
+    "PoolVote",
     # Disease & Outbreak
-    'DiseaseIncident', 'OutbreakZone', 'OutbreakAlert',
-    'MigrationVector', 'ContainmentZone',
+    "DiseaseIncident",
+    "OutbreakZone",
+    "OutbreakAlert",
+    "MigrationVector",
+    "ContainmentZone",
     # Traceability
-    'SupplyBatch', 'CustodyLog', 'QualityGrade', 'BatchStatus',
+    "SupplyBatch",
+    "CustodyLog",
+    "QualityGrade",
+    "BatchStatus",
     # Insurance
-    'InsurancePolicy', 'LegacyClaim', 'RiskScoreHistory', 'DynamicPremiumLog', 'RiskFactorSnapshot',
-    'CropPolicy', 'ClaimRequest', 'PayoutLedger', 'AdjusterNote', 'ParametricAutoSettlement',
-    'CropPolicy', 'ClaimRequest', 'PayoutLedger', 'AdjusterNote',
+    "InsurancePolicy",
+    "LegacyClaim",
+    "RiskScoreHistory",
+    "DynamicPremiumLog",
+    "RiskFactorSnapshot",
+    "CropPolicy",
+    "ClaimRequest",
+    "PayoutLedger",
+    "AdjusterNote",
+    "ParametricAutoSettlement",
+    "CropPolicy",
+    "ClaimRequest",
+    "PayoutLedger",
+    "AdjusterNote",
     # Community
-    'ForumCategory', 'ForumThread', 'PostComment', 'Upvote', 'UserReputation',
-    'Question', 'Answer', 'KnowledgeVote', 'Badge', 'UserBadge', 'UserExpertise',
+    "ForumCategory",
+    "ForumThread",
+    "PostComment",
+    "Upvote",
+    "UserReputation",
+    "Question",
+    "Answer",
+    "KnowledgeVote",
+    "Badge",
+    "UserBadge",
+    "UserExpertise",
     # Equipment & Rental
-    'Equipment', 'RentalBooking', 'AvailabilityCalendar', 'PaymentEscrow',
+    "Equipment",
+    "RentalBooking",
+    "AvailabilityCalendar",
+    "PaymentEscrow",
     # Farm
-    'Farm', 'FarmMember', 'FarmAsset', 'FarmRole',
+    "Farm",
+    "FarmMember",
+    "FarmAsset",
+    "FarmRole",
     # Alerts
-    'Alert', 'AlertPreference',
+    "Alert",
+    "AlertPreference",
     # Audit
-    'AuditLog', 'UserSession', 'MediaPayload',
+    "AuditLog",
+    "UserSession",
+    "MediaPayload",
     # Weather
-    'WeatherData', 'CropAdvisory', 'AdvisorySubscription', 'RiskTrigger',
-    'ClimateTelemetryEvent', 'ForceMajeureAlert', 'ParametricPolicyTrigger',
+    "WeatherData",
+    "CropAdvisory",
+    "AdvisorySubscription",
+    "RiskTrigger",
+    "ClimateTelemetryEvent",
+    "ForceMajeureAlert",
+    "ParametricPolicyTrigger",
     # Sustainability & ESG
-    'CarbonPractice', 'CreditLedger', 'AuditRequest', 'CarbonLedger', 'EmissionSource',
-    'SustainabilityScore', 'ESGMarketListing',
+    "CarbonPractice",
+    "CreditLedger",
+    "AuditRequest",
+    "CarbonLedger",
+    "EmissionSource",
+    "SustainabilityScore",
+    "ESGMarketListing",
     # Procurement
-    'VendorProfile', 'ProcurementItem', 'BulkOrder', 'OrderEvent',
+    "VendorProfile",
+    "ProcurementItem",
+    "BulkOrder",
+    "OrderEvent",
     # Irrigation & Water
-    'IrrigationZone', 'SensorLog', 'ValveStatus', 'IrrigationSchedule',
-    'AquiferLevel', 'WaterRightsQuota',
+    "IrrigationZone",
+    "SensorLog",
+    "ValveStatus",
+    "IrrigationSchedule",
+    "AquiferLevel",
+    "WaterRightsQuota",
     # Processing & Grading
-    'ProcessingBatch', 'StageLog', 'QualityCheck', 'ProcessingStage',
-    'SpectralScanData', 'DynamicGradeAdjustment',
+    "ProcessingBatch",
+    "StageLog",
+    "QualityCheck",
+    "ProcessingStage",
+    "SpectralScanData",
+    "DynamicGradeAdjustment",
     # Machinery
-    'EngineHourLog', 'MaintenanceCycle', 'DamageReport', 'RepairOrder',
-    'AssetValueSnapshot', 'ComponentWearMap', 'MaintenanceEscrow',
+    "EngineHourLog",
+    "MaintenanceCycle",
+    "DamageReport",
+    "RepairOrder",
+    "AssetValueSnapshot",
+    "ComponentWearMap",
+    "MaintenanceEscrow",
     # Soil & Carbon Sequestration
-    'SoilTest', 'FertilizerRecommendation', 'ApplicationLog',
-    'RegenerativeFarmingLog', 'CarbonMintEvent',
+    "SoilTest",
+    "FertilizerRecommendation",
+    "ApplicationLog",
+    "RegenerativeFarmingLog",
+    "CarbonMintEvent",
     # Finance
-    'RepaymentSchedule', 'PaymentHistory', 'DefaultRiskScore', 'CollectionNote',
-    'FarmBalanceSheet', 'SolvencySnapshot', 'ProfitabilityIndex',
+    "RepaymentSchedule",
+    "PaymentHistory",
+    "DefaultRiskScore",
+    "CollectionNote",
+    "FarmBalanceSheet",
+    "SolvencySnapshot",
+    "ProfitabilityIndex",
     # Warehouse
-    'WarehouseLocation', 'StockItem', 'StockMovement', 'ReconciliationLog',
+    "WarehouseLocation",
+    "StockItem",
+    "StockMovement",
+    "ReconciliationLog",
     # Climate
-    'ClimateZone', 'SensorNode', 'TelemetryLog', 'AutomationTrigger',
+    "ClimateZone",
+    "SensorNode",
+    "TelemetryLog",
+    "AutomationTrigger",
     # Labor
-    'WorkerProfile', 'WorkShift', 'HarvestLog', 'PayrollEntry', 'LaborROIHistory',
+    "WorkerProfile",
+    "WorkShift",
+    "HarvestLog",
+    "PayrollEntry",
+    "LaborROIHistory",
     # Logistics
-    'DriverProfile', 'DeliveryVehicle', 'TransportRoute', 'FuelLog',
-    'PhytoSanitaryCertificate', 'FreightEscrow', 'CustomsCheckpoint', 'GPSTelemetry',
+    "DriverProfile",
+    "DeliveryVehicle",
+    "TransportRoute",
+    "FuelLog",
+    "PhytoSanitaryCertificate",
+    "FreightEscrow",
+    "CustomsCheckpoint",
+    "GPSTelemetry",
     # Transparency & Barter
     # Transparency & Barter
-    'PhytoSanitaryCertificate', 'FreightEscrow', 'CustomsCheckpoint', 'GPSTelemetry',
-    'Alert', 'AlertPreference',
-    'AuditLog', 'UserSession',
-    'MediaPayload',
-    'ProduceReview', 'PriceAdjustmentLog',
-    'BarterTransaction', 'BarterResource', 'ResourceValueIndex',
+    "PhytoSanitaryCertificate",
+    "FreightEscrow",
+    "CustomsCheckpoint",
+    "GPSTelemetry",
+    "Alert",
+    "AlertPreference",
+    "AuditLog",
+    "UserSession",
+    "MediaPayload",
+    "ProduceReview",
+    "PriceAdjustmentLog",
+    "BarterTransaction",
+    "BarterResource",
+    "ResourceValueIndex",
     # Reliability & Market
-    'ReliabilityLog', 'ForwardContract', 'PriceHedgingLog',
+    "ReliabilityLog",
+    "ForwardContract",
+    "PriceHedgingLog",
     # Circular Economy & Biomass Energy
-    'WasteInventory', 'BioEnergyOutput', 'CircularCredit',
+    "WasteInventory",
+    "BioEnergyOutput",
+    "CircularCredit",
     # Carbon Trading Escrow (L3-1642)
-    'CarbonTradeEscrow', 'EscrowAuditLog', 'CarbonCreditWallet',
+    "CarbonTradeEscrow",
+    "EscrowAuditLog",
+    "CarbonCreditWallet",
     # Double-Entry Ledger
-    'LedgerAccount', 'LedgerTransaction', 'LedgerEntry',
-    'FXValuationSnapshot', 'Vault', 'VaultCurrencyPosition', 'FXRate',
-    'AccountType', 'EntryType', 'TransactionType',
+    "LedgerAccount",
+    "LedgerTransaction",
+    "LedgerEntry",
+    "FXValuationSnapshot",
+    "Vault",
+    "VaultCurrencyPosition",
+    "FXRate",
+    "AccountType",
+    "EntryType",
+    "TransactionType",
+    # Government Schemes
+    "GovernmentScheme",
+    "SchemeApplication",
+    "SchemeReminder",
 ]
